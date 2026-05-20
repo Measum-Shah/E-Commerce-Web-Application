29609117f4a231f562d27aad5ed8ef5945dc426d
Measum-Shah <shahmeasum008@gmail.com>
2026-05-21 02:12:12 +0500

    bug Resolved of deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,11 @@ import env from "./env.js";
 
 const connectDB = async () => {
   try {
-    // if (!env.MONGO_URI) {
-    //   throw new Error("MONGO_URI is missing in .env");
-    // }
+    if (!env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing in .env");
+    }
 
-    const conn = await mongoose.connect("mongodb://localhost:27017/premier");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
