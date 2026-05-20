9a1d9ce340de30a16bc2b23883440087f06f2461
Measum-Shah <shahmeasum008@gmail.com>
2026-05-21 02:29:12 +0500

    bug Resolved of deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,8 +15,9 @@ import contactRoutes from "./routes/contact.routes.js";
 
 const corsOptions = {
   // Allow only your frontend origin
-  origin: 'http://localhost:5173',
-  
+  // origin: 'http://localhost:5173',
+  origin:'https://e-commerce-web-application-aun5.onrender.com',
+   
   // Good practice for professional APIs
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
